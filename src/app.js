3752f72303340291d2b40e810bb4da703e776585
Haeun Kim <haeun159951@gmail.com>
2022-05-23 11:26:25 -0400

    Refactored files using the response.js functions

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const pino = require('pino-http')({
 });
 const passport = require('passport');
 const authorization = require('./authorization');
+const { createErrorResponse } = require('./response');
 
 // Create an express app instance we can use to attach middleware and HTTP routes
 const app = express();
@@ -34,13 +35,7 @@ app.use('/', require('./routes'));
 
 // Add 404 middleware to handle any requests for resources that can't be found can't be found
 app.use((req, res) => {
-  res.status(404).json({
-    status: 'error',
-    error: {
-      message: 'not found',
-      code: 404,
-    },
-  });
+  res.status(404).json(createErrorResponse('404', 'Not Found'));
 });
 
 // Add error-handling middleware to deal with anything else
@@ -56,13 +51,7 @@ app.use((err, req, res, next) => {
     logger.error({ err }, `Error processing request`);
   }
 
-  res.status(status).json({
-    status: 'error',
-    error: {
-      message,
-      code: status,
-    },
-  });
+  res.status(status).json(createErrorResponse(status, message));
 });
 
 // Export our `app` so we can access it in server.js
