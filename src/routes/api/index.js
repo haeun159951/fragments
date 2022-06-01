7bca21b2122e86e3e004a9c07bd2d7ac69f912f6
Haeun Kim <haeun159951@gmail.com>
2022-06-01 19:50:38 -0400

    fixed the proper file name and installed nanoid and content-type

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,13 +1,33 @@
 /**
  * The main entry-point for the v1 version of the fragments API.
  */
+const { Fragment } = require('../../model/fragment');
+const contentType = require('content-type');
+
 const express = require('express');
 
 // Create a router on which to mount our API endpoints
 const router = express.Router();
 
+// Support sending various Content-Types on the body up to 5M in size
+const rawBody = () =>
+  express.raw({
+    inflate: true,
+    limit: '5mb',
+    type: (req) => {
+      // See if we can parse this content type. If we can, `req.body` will be
+      // a Buffer (e.g., `Buffer.isBuffer(req.body) === true`). If not, `req.body`
+      // will be equal to an empty Object `{}` and `Buffer.isBuffer(req.body) === false`
+      const { type } = contentType.parse(req);
+      return Fragment.isSupportedType(type);
+    },
+  });
+
 // Define our first route, which will be: GET /v1/fragments
 router.get('/fragments', require('./get'));
+
+// Use a raw body parser for POST, which will give a `Buffer` Object or `{}` at `req.body`
+router.post('/fragments', rawBody(), require('./post'));
 
 // Other routes will go here later on...
 
