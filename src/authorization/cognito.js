16e4285660058e918ea307733694b684d4c5890d
Haeun Kim <haeun159951@gmail.com>
2022-06-07 23:48:00 -0400

    Add hash middleware and modified cognito basic-auth memory-db

--- src/authorization/cognito.js
+++ src/authorization/cognito.js
@@ -4,7 +4,7 @@
 // Identity Token provided by Cognito. The token will be
 // parsed from the Authorization header (i.e., Bearer Token).
 
-const passport = require('passport');
+const authorize = require('./authorize-middleware');
 const BearerStrategy = require('passport-http-bearer').Strategy;
 const { CognitoJwtVerifier } = require('aws-jwt-verify');
 
@@ -55,4 +55,4 @@ module.exports.strategy = () =>
     }
   });
 
-module.exports.authenticate = () => passport.authenticate('bearer', { session: false });
+module.exports.authenticate = () => authorize('bearer');
