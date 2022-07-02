a7659270f5e6e0eed3ce8800e8262a8ea7b53ddf
Haeun Kim <haeun159951@gmail.com>
2022-07-02 10:06:15 -0400

    added get-id.test.js

--- tests/unit/get-id.test.js
+++ tests/unit/get-id.test.js
@@ -1,7 +1,6 @@
 // tests/unit/get.test.js
 
 const request = require('supertest');
-
 const app = require('../../src/app');
 
 describe('GET by Id /v1/fragments/:id', () => {
@@ -18,22 +17,23 @@ describe('GET by Id /v1/fragments/:id', () => {
 
   // authenticated users get a fragments id after creating a fragment
   test('authenticated users get a fragments id after creating a fragment', async () => {
+    const data = Buffer.from('This is fragment');
     const postRes = await request(app)
       .post('/v1/fragments')
       .auth('user1@email.com', 'password1')
-      .set('Content-type', 'text/plain')
-      .send('Fragment 1');
-    expect(postRes.statusCode).toBe(201);
-    expect(postRes.body.status).toBe('ok');
-    const id = postRes.body.fragment.id;
+      .set('Content-Type', 'text/plain')
+      .send(data);
 
-    const res = await request(app).get(`/v1/fragments/${id}`).auth('user1@email.com', 'password1');
+    const body = JSON.parse(postRes.text);
+    console.log(body.fragment.id);
 
-    expect(res.statusCode).toBe(200);
+    const id = body.fragment.id;
 
-    console.log(res.text);
-
-    //expect(res.body).toEqual('Fragment 1');
+    const getRes = await request(app)
+      .get(`/v1/fragments/${id}`)
+      .auth('user1@email.com', 'password1');
+    expect(getRes.statusCode).toBe(200);
+    expect(getRes.text).toBe(data.toString());
   });
 
   //authenticated user cannot get the fragment with wrong id
@@ -43,13 +43,54 @@ describe('GET by Id /v1/fragments/:id', () => {
       .auth('user2@email.com', 'password2')
       .set('Content-type', 'text/plain')
       .send('Fragment 1');
-    expect(postRes.statusCode).toBe(201);
-    expect(postRes.body.status).toBe('ok');
-    const id2 = postRes.body.fragment.id;
 
-    const get = await request(app).get(`/v1/fragments/${id2}`).auth('user1@email.com', 'password1');
+    const body = JSON.parse(postRes.text);
+
+    expect(postRes.statusCode).toBe(201);
+    expect(body.status).toBe('ok');
+    const id = body.fragment.id;
+
+    const get = await request(app).get(`/v1/fragments/${id}`).auth('user1@email.com', 'password1');
     expect(get.statusCode).toBe(404);
   });
 
-  // TODO: we'll need to add tests to check the contents of the fragments array later
+  // .md to .html conversion test
+  test('authenticated user can convert .md to .html', async () => {
+    const postRes = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-type', 'text/markdown')
+      .send('This is markdown');
+
+    const body = JSON.parse(postRes.text);
+    expect(postRes.statusCode).toBe(201);
+    expect(body.status).toBe('ok');
+    const id = body.fragment.id;
+
+    const getRes = await request(app)
+      .get(`/v1/fragments/${id}.html`)
+      .auth('user1@email.com', 'password1');
+    expect(getRes.statusCode).toBe(200);
+    expect(getRes.headers['content-type']).toEqual('text/html; charset=utf-8');
+    expect(getRes.text).toEqual('<p>This is markdown</p>\n');
+  });
+
+  test('authenticated user cannot convert to unsupported type and it returns error 415', async () => {
+    const postRes = await request(app)
+      .post('/v1/fragments')
+      .auth('user2@email.com', 'password2')
+      .set('Content-Type', 'text/plain')
+      .send('This is fragment');
+
+    const body = JSON.parse(postRes.text);
+    expect(postRes.statusCode).toBe(201);
+    expect(body.status).toBe('ok');
+    const id = body.fragment.id;
+
+    const getRes = await request(app)
+      .get(`/v1/fragments/${id}.png`)
+      .auth('user2@email.com', 'password2');
+
+    expect(getRes.statusCode).toBe(415);
+  });
 });
